2bc5e45cc6d07f27bd9f569b2850a6c374d488fe
Corrado Parisi <corradoparisi@hotmail.com>
2020-05-05 11:36:35 +0200

    Initialize users for development

--- grails-app/init/webec/BootStrap.groovy
+++ grails-app/init/webec/BootStrap.groovy
@@ -1,9 +1,33 @@
 package webec
 
+import grails.util.Environment
+
 class BootStrap {
 
     def init = { servletContext ->
+        if (Environment.current == Environment.PRODUCTION) {
+            return
+        }
+
+        Role adminRole = save(Role.findOrCreateWhere(authority: Role.ADMIN))
+        Role userRole = save(Role.findOrCreateWhere(authority: Role.USER))
+
+        User user = save(new User(username: 'user', password: 'user'))
+        User admin = save(new User(username: 'admin', password: 'admin'))
+
+        UserRole.create(user, userRole, true)
+        UserRole.create(admin, adminRole, true)
+
+        // plausibility check
+        assert Role.count() == 2
+        assert User.count() == 2
+        assert UserRole.count() == 2
     }
+
+    static save(domainObject) {
+        domainObject.save(failOnError: true)
+    }
+
     def destroy = {
     }
 }
