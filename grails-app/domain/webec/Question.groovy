7dd85ad6fe2ad24c616ff73d7d21257a0ff79d5b
Corrado Parisi <corradoparisi@hotmail.com>
2020-05-06 10:17:48 +0200

    Update Question domain class

--- grails-app/domain/webec/Question.groovy
+++ grails-app/domain/webec/Question.groovy
@@ -2,6 +2,22 @@ package webec
 
 class Question {
 
+    String questionText
+    String correct
+    String second
+    String third
+    String fourth
+
+
+    String toString() {
+        return "$questionText: correct: $correct other answers: ($second, $third, $fourth)"
+    }
+
     static constraints = {
+        questionText(blank: false, nullable: false)
+        correct(blank: false, nullable: false)
+        second(blank: false, nullable: false)
+        third(blank: false, nullable: false)
+        fourth(blank: false, nullable: false)
     }
 }
