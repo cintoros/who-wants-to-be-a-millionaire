b92b573e8f31c982be384339e99bae64a93dcf93
Corrado Parisi <corradoparisi@hotmail.com>
2020-05-16 13:01:43 +0200

    Generate unique QuestionSet service method

--- grails-app/services/webec/QuestionService.groovy
+++ grails-app/services/webec/QuestionService.groovy
@@ -5,7 +5,23 @@ import grails.gorm.transactions.Transactional
 @Transactional
 class QuestionService {
 
-    def serviceMethod() {
-
+    /**
+     * Generates a random set of unique questions by the specified <code>size</code>
+     *
+     * If there are not enough questions in the database, or any at all,
+     * an empty list is returned.
+     *
+     * @param size the size of the questions set, default value: 15
+     * @return a list of unique and random questions
+     */
+    def uniqueQuestionSet(int size = 15) {
+        def questions = Question.all
+        if (questions.size() < size) {
+            Collections.shuffle(questions);
+            questions
+        } else {
+            Collections.shuffle(questions);
+            questions.subList(0, size)
+        }
     }
 }
