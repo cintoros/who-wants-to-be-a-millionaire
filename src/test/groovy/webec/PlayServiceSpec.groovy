68fdecf7506354204ecf7085f727c9c9e4d1d59c
Corrado Parisi <corradoparisi@hotmail.com>
2020-05-25 22:47:02 +0200

    Add integration test

--- src/test/groovy/webec/PlayServiceSpec.groovy
+++ src/test/groovy/webec/PlayServiceSpec.groovy
@@ -1,18 +1,33 @@
 package webec
 
+import grails.gorm.transactions.Rollback
+import grails.testing.mixin.integration.Integration
 import grails.testing.services.ServiceUnitTest
 import spock.lang.Specification
 
-class PlayServiceSpec extends Specification implements ServiceUnitTest<PlayService>{
+@Rollback
+@Integration
+class PlayServiceSpec extends Specification implements ServiceUnitTest<PlayService> {
 
     def setup() {
+      // empty
     }
 
     def cleanup() {
+        // empty
     }
 
-//    void "test something"() {
-//        expect:"fix me"
-//            true == false
-//    }
+    void 'test removeTwoWrongAnswers'() {
+        when:
+        def updatedQuestion = service.removeTwoWrongAnswersFromQuestion(1)
+
+        then:
+        updatedQuestion.correct != null
+        updatedQuestion.questionText != null
+
+        updatedQuestion.second == null
+        updatedQuestion.third == null
+
+        updatedQuestion.fourth != null
+    }
 }
