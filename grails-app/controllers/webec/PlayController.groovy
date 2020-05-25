3c71f117f66f0ce52a245393e4ce1bc57baefa62
Corrado Parisi <corradoparisi@hotmail.com>
2020-05-25 12:01:36 +0200

    Improve game error handling

--- grails-app/controllers/webec/PlayController.groovy
+++ grails-app/controllers/webec/PlayController.groovy
@@ -1,12 +1,22 @@
 package webec
 
 class PlayController {
-    def playService //FIXME use
     def questionService
     Game game
     Question lastQuestion
 
+    /**
+     * Will create a new controller instance per user browser session.
+     * A different browser will have a different session and therefore
+     * an independent game.
+     */
+    static scope = "session"
 
+
+    /**
+     * Handles the interactive 15 questions game.
+     * @return the game entity to be displayed to the view.
+     */
     def index() {
         String answer = params.get("answer")
         String joker = params.get("joker")
@@ -14,7 +24,13 @@ class PlayController {
         if (answer == null || answer.isEmpty()) {
             game = newGame()
         } else {
-            def currentQuestion = game.questions[game.current]
+            if(game == null || game.questions == null) {
+                // start new game
+                redirect(uri:'/')
+                return
+            }
+
+            def currentQuestion = game?.questions[game.current]
             if (currentQuestion == null || currentQuestion.correct == null || currentQuestion?.id == lastQuestion?.id) {
                 // user cheated by pressing back button!
                 render(view: 'cheat')
@@ -42,19 +58,16 @@ class PlayController {
     }
 
     private Game newGame() {
-        new Game(current: 0, questions: questionService.uniqueQuestionSet(), isFinished: false)
+        new Game(current: 0, questions: questionService.uniqueQuestionSet())
     }
 
     def moderator() {
         [questions: questionService.uniqueQuestionSet()]
     }
-
-
 }
 
 class Game {
     int current
     List<Question> questions
-    boolean isFinished
     boolean isJokerUsed
 }
--- grails-app/controllers/webec/QuestionController.groovy
+++ grails-app/controllers/webec/QuestionController.groovy
@@ -5,6 +5,10 @@ class QuestionController {
     static scaffold = Question
     def questionService
 
+    /**
+     * Returns all questions.
+     * @return the question list to be displayed to the view.
+     */
     def index() {
         [questions: questionService.all()]
     }
