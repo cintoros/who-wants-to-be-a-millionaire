cab8815c4d7c445384c6222bbe7b45da95122ac2
Elias Schorr <elias.schorr@fhnw.ch>
2020-12-20 18:43:56 +0100

    enable endpoints

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,5 +1,3 @@
-
-
 // Added by the Spring Security Core plugin:
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'webec.User'
 grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'webec.UserRole'
@@ -8,28 +6,30 @@ grails.plugin.springsecurity.authority.className = 'webec.Role'
 grails.plugin.springsecurity.securityConfigType = 'Annotation'
 
 grails.plugin.springsecurity.controllerAnnotations.staticRules = [
-	[pattern: '/',               access: ['permitAll']],
-	[pattern: '/error',          access: ['permitAll']],
-	[pattern: '/index',          access: ['permitAll']],
-	[pattern: '/index.gsp',      access: ['permitAll']],
-	[pattern: '/play/**',        access: ['permitAll']],
-	[pattern: '/play**',         access: ['permitAll']],
-	[pattern: '/question/**',    access: ['ROLE_ADMIN']],
-	[pattern: '/shutdown',       access: ['permitAll']],
-	[pattern: '/assets/**',      access: ['permitAll']],
-	[pattern: '/**/js/**',       access: ['permitAll']],
-	[pattern: '/**/css/**',      access: ['permitAll']],
-	[pattern: '/**/images/**',   access: ['permitAll']],
-	[pattern: '/**/favicon.ico', access: ['permitAll']]
+        [pattern: '/', access: ['permitAll']],
+        [pattern: '/error', access: ['permitAll']],
+        [pattern: '/index', access: ['permitAll']],
+        [pattern: '/index.gsp', access: ['permitAll']],
+        [pattern: '/play/**', access: ['permitAll']],
+        [pattern: '/play**', access: ['permitAll']],
+        [pattern: '/question/**', access: ['ROLE_ADMIN']],
+        [pattern: '/actuator/**', access: ['ROLE_ADMIN']],
+        [pattern: '/manage/**', access: ['ROLE_ADMIN']],
+        [pattern: '/shutdown', access: ['permitAll']],
+        [pattern: '/assets/**', access: ['permitAll']],
+        [pattern: '/**/js/**', access: ['permitAll']],
+        [pattern: '/**/css/**', access: ['permitAll']],
+        [pattern: '/**/images/**', access: ['permitAll']],
+        [pattern: '/**/favicon.ico', access: ['permitAll']]
 ]
 
 grails.plugin.springsecurity.filterChain.chainMap = [
-	[pattern: '/assets/**',      filters: 'none'],
-	[pattern: '/**/js/**',       filters: 'none'],
-	[pattern: '/**/css/**',      filters: 'none'],
-	[pattern: '/**/images/**',   filters: 'none'],
-	[pattern: '/**/favicon.ico', filters: 'none'],
-	[pattern: '/**',             filters: 'JOINED_FILTERS']
+        [pattern: '/assets/**', filters: 'none'],
+        [pattern: '/**/js/**', filters: 'none'],
+        [pattern: '/**/css/**', filters: 'none'],
+        [pattern: '/**/images/**', filters: 'none'],
+        [pattern: '/**/favicon.ico', filters: 'none'],
+        [pattern: '/**', filters: 'JOINED_FILTERS']
 ]
 
 // logout and redirect
