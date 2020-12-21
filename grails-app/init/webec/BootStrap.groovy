2987f408a9063ae66495b0779ccd216f2fa4b5c2
Elias Schorr <elias.schorr@fhnw.ch>
2020-12-21 09:58:50 +0100

    fix idiotic bootstrap code

--- grails-app/init/webec/BootStrap.groovy
+++ grails-app/init/webec/BootStrap.groovy
@@ -1,35 +1,27 @@
 package webec
 
 class BootStrap {
-
     def utilityService
-
     def init = { servletContext ->
         // comment out production questions for easier development
-        utilityService.loadProductionQuestions()
-
-        Role adminRole = utilityService.save(Role.findOrCreateWhere(authority: Role.ADMIN))
-        Role playerRole = utilityService.save(Role.findOrCreateWhere(authority: Role.PLAYER))
-
-        // must use longer passwords due to configured password constrains.
-        // username cannot be user due to configured username constraints.
-        User player = utilityService.save(new User(username: 'player', password: 'player123'))
-        User admin = utilityService.save(new User(username: 'admin', password: 'admin123'))
-
-        UserRole.create(player, playerRole, true)
-        UserRole.create(admin, adminRole, true)
-
-        // plausibility check
-        assert Role.count() == 2
-        assert User.count() == 2
-        assert UserRole.count() == 2
-
-        // comment in dev questions for easier development
-        //utilityService.loadDevelopmentQuestions()
+        if (Question.count() <= 0) {
+            utilityService.loadProductionQuestions()
+        }
+        if (User.count() <= 0) {
+            Role adminRole = utilityService.save(Role.findOrCreateWhere(authority: Role.ADMIN))
+            Role playerRole = utilityService.save(Role.findOrCreateWhere(authority: Role.PLAYER))
+            // must use longer passwords due to configured password constrains.
+            // username cannot be user due to configured username constraints.
+            User player = utilityService.save(new User(username: 'player', password: 'player123'))
+            User admin = utilityService.save(new User(username: 'admin', password: 'admin123'))
+            UserRole.create(player, playerRole, true)
+            UserRole.create(admin, adminRole, true)
+            // plausibility check
+            assert Role.count() == 2
+            assert User.count() == 2
+            assert UserRole.count() == 2
+        }
     }
-
-
     def destroy = {
-        // empty
     }
 }
